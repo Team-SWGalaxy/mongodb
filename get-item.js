abd0a4286ceab6e3e107ef57db3fd947547f1004
XLuoChen <1425145741@qq.com>
2016-06-09 17:29:28 +0800

    first commit get-item.js

---
+++ get-item.js
@@ -0,0 +1,31 @@
+var mongoClient = require('mongodb').MongoClient;
+var dbConnectStr = 'mongodb://localhost:27017/items';
+var scanf = require('scanf');
+
+var getItem = function (db, callback) {
+
+    var collection = db.collection('items');
+
+    console.log('请输入要查询的商品的barcode');
+    var barcode = scanf('%d');
+    var queryStr = {"barcode": barcode};
+
+    collection.findOne(queryStr, {'_id': 0}, function (err, result) {
+        if (err) {
+            throw err;
+        }
+        callback(result);
+    });
+};
+
+mongoClient.connect(dbConnectStr, function (err, db) {
+
+    if (err) {
+
+        throw err;
+    }
+    getItem(db, function (result) {
+        console.log(result);
+        db.close();
+    })
+});
