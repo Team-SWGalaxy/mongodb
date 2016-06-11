b2cb76374d92d738bc548d85493ddea563ef1ac6
XLuoChen <1425145741@qq.com>
2016-06-11 21:37:22 +0800

    modified startup file

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 var scanf = require('scanf');
-var addItems = require('./add-items');
 
-function operateItems() {
-    displayMenu();
-   
-    var chioce = scanf('%d');
-    if (chioce === 1){
-        addItems.addItems();
-    }
-    // console.log(chioce);
+// var addItems = require('./add-items');
+var deleteItem = require('./delete-item');
+var getAllItems = require('./get-all-items');
+var getItem = require('./get-item');
+var updateItem = require('./update-item');
+
+displayMenu();
+
+var chioce = scanf('%d');
+
+switch (chioce){
+    case 1: addItems(); break;
+    case 2: deleteItem(); break;
+    case 3: getAllItems(); break;
+    case 4:getItem(); break;
+    case 5: updateItem(); break;
+
 }
+
 
 function displayMenu() {
 
-    console.log( '请选择:\n' +
+    console.log('请选择要进行的操作:\n' +
         '1. 增加商品\n' +
         '2. 删除商品\n' +
-        '3. 查看商品信息\n' +
-        '4. 修改商品信息');
+        '3. 查看所有商品信息\n' +
+        '4. 查看指定商品信息\n' +
+        '5. 修改商品信息');
 }
 
-operateItems();
+// operateItems();
