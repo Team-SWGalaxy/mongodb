6ae1a8d7ed87fcbefb200629f9724167c40b910e
XLuoChen <1425145741@qq.com>
2016-06-11 21:36:25 +0800

    modified get-item

--- get-item.js
+++ get-item.js
@@ -2,30 +2,36 @@ var mongoClient = require('mongodb').MongoClient;
 var dbConnectStr = 'mongodb://localhost:27017/items';
 var scanf = require('scanf');
 
-var getItem = function (db, callback) {
+var getItem = function () {
 
-    var collection = db.collection('items');
 
-    console.log('请输入要查询的商品的barcode');
-    var barcode = scanf('%d');
-    var queryStr = {"barcode": barcode};
+    var findItem = function (db, callback) {
 
-    collection.findOne(queryStr, {'_id': 0}, function (err, result) {
+        var collection = db.collection('items');
+
+        console.log('请输入要查询的商品的barcode');
+        var barcode = scanf('%d');
+        var queryStr = {"barcode": barcode};
+
+        collection.findOne(queryStr, {'_id': 0}, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+    };
+
+    mongoClient.connect(dbConnectStr, function (err, db) {
+
         if (err) {
+
             throw err;
         }
-        callback(result);
+        findItem(db, function (result) {
+            console.log(result);
+            db.close();
+        })
     });
 };
 
-mongoClient.connect(dbConnectStr, function (err, db) {
-
-    if (err) {
-
-        throw err;
-    }
-    getItem(db, function (result) {
-        console.log(result);
-        db.close();
-    })
-});
+module.exports = getItem;
