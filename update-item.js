f424b0977305ff0dbbbaf97790391663fe9870c6
XLuoChen <1425145741@qq.com>
2016-06-11 17:29:57 +0800

    first add update-item.js

---
+++ update-item.js
@@ -0,0 +1,53 @@
+var mongoClient = require('mongodb').MongoClient;
+var dbConnectStr = 'mongodb://localhost:27017/items';
+var scanf = require('scanf');
+
+var updateItem = function (db, callback) {
+
+    var collection = db.collection('items');
+    
+    console.log('请输入要修改的商品的barcode');
+    var queryStr = {"barcode":scanf('%d')};
+    
+    console.log('请输入要修改的信息,并以逗号隔开(不填则仍为原值)：\n' +
+        'name  price  unit  demo(选填)');
+    var updateData = scanf('%s').split(',');
+
+    var data = modifyItem(updateData);
+    
+    collection.update(queryStr,{$set:data}, function (err, result) {
+        if (err){
+            throw err;
+        }
+        callback(result);
+    })
+};
+
+mongoClient.connect(dbConnectStr, function (err, db) {
+
+    if (err) {
+        throw err;
+    }
+    updateItem(db, function (result) {
+        console.log(result);
+        db.close();
+    })
+});
+
+function modifyItem(updatedata) {
+    var data = {};
+    if(updatedata[0]){
+        data.name = updatedata[0];
+    }
+    if (updatedata[1]){
+        data.price = parseFloat(updatedata[1]);
+    }
+    if (updatedata[2]){
+        data.unit = updatedata[2];
+    }
+    if (updatedata[3]){
+        data.demo = updatedata[3];
+    }
+
+    return data;
+}
