1dc3f47c6adc4ee9ec0486120f2169537bc23c26
XLuoChen <1425145741@qq.com>
2016-06-11 21:35:26 +0800

    modified delete-item

--- delete-item.js
+++ delete-item.js
@@ -2,31 +2,34 @@ var mongoClient = require('mongodb').MongoClient;
 var dbConnectStr = 'mongodb://localhost:27017/items';
 var scanf = require('scanf');
 
-var deleteItem = function (db, callback) {
+var deleteItem = function () {
 
-    var collection = db.collection('items');
+    var removeItem = function (db, callback) {
 
-    console.log('请输入要删除的商品的barcode');
-    var barcode = scanf('%d');
-    var queryStr = {"barcode": barcode};
-    
-    collection.remove(queryStr, function (err, result) {
+        var collection = db.collection('items');
+
+        console.log('请输入要删除的商品的barcode');
+        var barcode = scanf('%d');
+        var queryStr = {"barcode": barcode};
+
+        collection.remove(queryStr, function (err, result) {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            callback(result);
+        });
+    };
+
+    mongoClient.connect(dbConnectStr, function (err, db) {
         if (err) {
             console.log(err);
-            throw err;
         }
-        callback(result);
+        removeItem(db, function (result) {
+            console.log('删除成功');
+            db.close();
+        });
     });
 };
 
-mongoClient.connect(dbConnectStr, function (err, db) {
-    if (err) {
-        console.log(err);
-    }
-    deleteItem(db, function (result) {
-        console.log('删除成功');
-        db.close();
-    });
-});
-
-
+module.exports = deleteItem;
