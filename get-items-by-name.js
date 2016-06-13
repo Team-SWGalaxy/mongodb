65d68bcb649c2cf5c40ac9f6ac858775e27abd0c
XLuoChen <1425145741@qq.com>
2016-06-13 16:35:21 +0800

    realize get items by name

--- get-items-by-name.js
+++ get-items-by-name.js
@@ -1,34 +1,36 @@
 var mongoClient = require('mongodb').MongoClient;
-var dbconnectStr = 'mongodb://localhost:27017/items';
+var dbConnectStr = 'mongodb://localhost:27017/items';
 var scanf = require('scanf');
 
 var getItemsByName = function () {
-    
-    var findItemsByName = function (db, callback) {
+
+    var findItem = function (db, callback) {
 
         var collection = db.collection('items');
 
-        console.log('请输入商品名：');
+        console.log('请输入要查询的商品的名字');
         var name = scanf('%s');
-        var queryStr = {name:name};
+        var queryStr = {name: new RegExp("^.*"+name+".*$")};
 
-        collection.find(queryStr, function (err, result) {
-            if (err){
+        collection.find(queryStr, {'_id': 0}).toArray( function (err, result) {
+            if (err) {
                 throw err;
             }
             callback(result);
         });
     };
-    
-    mongoClient.connect(dbconnectStr, function (err, db) {
-        if (err){
+
+    mongoClient.connect(dbConnectStr, function (err, db) {
+
+        if (err) {
+
             throw err;
         }
-        findItemsByName(db, function (result) {
+        findItem(db, function (result) {
             console.log(result);
-        });
-        db.close();
-    })
+            db.close();
+        })
+    });
 };
 
 module.exports = getItemsByName;
